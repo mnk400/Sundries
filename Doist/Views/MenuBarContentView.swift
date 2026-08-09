15348a16747408a0e8d64057f5cf1d10471ccd4d
Manik Kumar <mnk_400@icloud.com>
2026-08-09 01:11:46 -0700

    Improve panel readability with frosted backing

--- Doist/Views/MenuBarContentView.swift
+++ Doist/Views/MenuBarContentView.swift
@@ -41,6 +41,7 @@ struct MenuBarContentView: View {
             footer
         }
         .frame(width: 536, height: 548)
+        .background(.thickMaterial)
         .animation(feedbackAnimation, value: taskStore.parsedDraft != nil)
         .animation(feedbackAnimation, value: taskStore.lastCompletedTask?.id)
         .onAppear {
